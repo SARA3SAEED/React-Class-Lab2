47a9b7c9309f5b0fe20498fc53f163ed1d94ec8d
SARA3SAEED <Swaretoo@gmail.com>
2024-06-08 02:11:04 +0300

    first commit

---
+++ introW/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Nav from './component/Nav'
+function App() {
+  
+
+  return (
+    <>
+
+    <Nav></Nav>
+
+    </>
+  )
+}
+
+export default App
---
+++ introW/src/component/Nav.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Nav() {
+  return (
+    <div>
+      <h1>Sara</h1>
+    </div>
+  )
+}
+
+
+
+
+
